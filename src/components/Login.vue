d131320866949df82ee8ce9a3247b22239667601
cesarfuentes <cesarfuentes051294@gmail.com>
2018-06-10 20:07:57 -0400

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="Login">
+		<h2>Bienvenido a Login</h2>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
